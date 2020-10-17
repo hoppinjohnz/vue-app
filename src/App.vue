b03e8ea9c4f8bb68465ff4ae5f7693ab3ca13a13
hoppinjohnz <johnzhu00@gmail.com>
2020-10-16 21:23:46 -0400

    refactor to use data for employees and loop to show the table

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <hello-world msg="Hi John!"/>
 
     <h1>Employees</h1>
-    <employee-table />
+    <employee-table :employees="this.employees"/>
   </div>
 </template>
 
@@ -17,6 +17,27 @@
     components: {
       HelloWorld,
       EmployeeTable,
+    },
+    data() {
+      return {
+        employees: [
+          {
+            id: 1,
+            name: 'Richard Hendricks',
+            email: 'richard@piedpiper.com',
+          },
+          {
+            id: 2,
+            name: 'Bertram Gilfoyle',
+            email: 'gilfoyle@piedpiper.com',
+          },
+          {
+            id: 3,
+            name: 'Dinesh Chugtai',
+            email: 'dinesh@piedpiper.com',
+          },
+        ],
+      }
     },
   }
 </script>
